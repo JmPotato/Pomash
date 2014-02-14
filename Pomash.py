446f43c7cd1a0e950ff769ed5a924ae9357f260e
JmPotato <ghzpotato@gmail.com>
2014-02-14 21:51:46 +0800

    Add authenticated

--- Pomash.py
+++ Pomash.py
@@ -10,11 +10,12 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             ("/", HomeHandler),
+            ("/login", LoginHandler),
+            ("/admin", AdminHandler),
         ]
         settings = dict(
             static_path = os.path.join(os.path.dirname(__file__), "static"),
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
-            xsrf_cookies = True,
             cookie_secret = cookie_secret,
             login_url = "/login",
             debug = DeBug
@@ -23,14 +24,68 @@ class Application(tornado.web.Application):
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_current_user(self):
-        token = self.get_secure_cookie("token")
-        if not token:
-            return None
-        username = token.split("^")[0]
+        username = self.get_secure_cookie("username")
         if not username:
-            return None
+            return Nones
         return username
 
 class HomeHandler(BaseHandler):
     def get(self):
-        self.render("home.html", title = blog_name, blog_url = blog_url, blog_name = blog_name, articlesList = get_articles(4))
+        self.render("home.html",
+            title = blog_name,
+            blog_url = blog_url,
+            blog_name = blog_name,
+            articlesList = get_articles(5)
+            )
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        if self.get_current_user():
+            if verify_token(self.get_current_user(), self.get_secure_cookie("token")):
+                self.redirect("/admin")
+                return
+            else:
+                self.render("login.html",
+                    title = blog_name,
+                    blog_url = blog_url,
+                    blog_name = blog_name,
+                    not_login = True
+                    )
+        else:
+            self.render("login.html",
+                title = blog_name,
+                blog_url = blog_url,
+                blog_name = blog_name,
+                not_login = False
+                )
+
+    def post(self):
+        username = self.get_argument("username", None)
+        password = self.get_argument("password", None)
+        if verify_user(username, to_md5(password)):
+            token = make_token(username)
+            updata_token(username, token)
+            self.set_secure_cookie("token", token)
+            self.set_secure_cookie("username", username)
+            self.redirect("/admin")
+            return
+        else:
+            self.render("login.html",
+                title = blog_name,
+                blog_url = blog_url,
+                blog_name = blog_name,
+                not_login = True
+                )
+
+class AdminHandler(BaseHandler):
+
+    @tornado.web.authenticated
+    def get(self):
+        self.render("admin.html",
+            title = blog_name,
+            blog_url = blog_url,
+            blog_name = blog_name,
+            blog_author = blog_author,
+            articlesList = get_all_articles()
+            )
+        
--- libs/models.py
+++ libs/models.py
@@ -2,6 +2,7 @@ import os
 import datetime
 
 from tools import *
+from utils import *
 
 class DatabaseError(Exception):
     def __init__(self, content):
@@ -20,12 +21,16 @@ def get_articles(count):
     articles = db.query("SELECT * FROM articles ORDER BY id DESC LIMIT ?;",count)
     return articles
 
+def get_all_articles():
+    articles = db.query("SELECT * FROM articles;")
+    return articles
+
 def get_article_by_category(category_name):
     articles = db.get("SELECT articles_id FROM category WHERE name = ?;", category_name)
     return articles
 
 def update_article(id, **kwargs):
-    sql = '''UPDATE articles SET title=?, content=?, category=?, WHERE id=?;'''
+    sql = '''UPDATE articles SET title=?, content=?, category=? WHERE id=?;'''
     db.execute(sql, kwargs["title"], kwargs["content"], kwargs["category"], id)
     return True
 
@@ -39,7 +44,6 @@ def creat_category(name):
     category_id = db.execute('''INSERT INTO category (name) VALUES (?);''', name)
     return category_id
 
-
 def delete_article(id):
     db.execute("DELETE FROM articles WHERE id=?;", id)
     db.execute("DELETE FROM category WHERE articles_id=?;", id)
@@ -47,4 +51,23 @@ def delete_article(id):
 
 def delete_category(category_name):
     db.execute("DELETE FROM category WHERE id=?;", category_name)
+    return True
+
+def verify_user(username, password_md5):
+    information = db.get("SELECT * FROM admin_config WHERE username = ?;", username)
+    if information.password == password_md5:
+        return True
+    else:
+        return False
+
+def verify_token(username, token):
+    information = db.get("SELECT * FROM admin_config WHERE username = ?;", username)
+    if information.token == token:
+        return True
+    else:
+        return False
+
+def updata_token(username, token):
+    sql = '''UPDATE admin_config SET token=? WHERE username=?;'''
+    db.execute(sql, token, username)
     return True
