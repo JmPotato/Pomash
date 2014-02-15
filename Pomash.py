c2b8228dbcdf6ba0b3438470cd49201cfe544883
JmPotato <ghzpotato@gmail.com>
2014-02-16 00:18:24 +0800

    rewrite render_string

--- Pomash.py
+++ Pomash.py
@@ -1,3 +1,4 @@
+import time
 import string
 import os.path
 import tornado.web
@@ -5,37 +6,35 @@ import tornado.web
 from settings import *
 from libs.utils import *
 from libs.models import *
+from libs.handler import *
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             ("/", HomeHandler),
             ("/login", LoginHandler),
+            ("/logout", LogoutHandler),
             ("/admin", AdminHandler),
+            ("/admin/edit/new", NewArticleHandler),
+            ("/admin/edit/([\d]+)", EditArticleHandler),
+            ("/admin/edit/delete/([\d]+)", DelArticleHandler),
         ]
         settings = dict(
             static_path = os.path.join(os.path.dirname(__file__), "static"),
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
+            blog_name = blog_name,
+            blog_url = blog_url,
             cookie_secret = cookie_secret,
             login_url = "/login",
             debug = DeBug
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 
-class BaseHandler(tornado.web.RequestHandler):
-    def get_current_user(self):
-        username = self.get_secure_cookie("username")
-        if not username:
-            return None
-        return username
-
 class HomeHandler(BaseHandler):
     def get(self):
         self.render("home.html",
             title = blog_name,
-            blog_url = blog_url,
-            blog_name = blog_name,
-            articlesList = get_articles(5)
+            articlesList = get_articles(5),
             )
 
 class LoginHandler(BaseHandler):
@@ -47,15 +46,11 @@ class LoginHandler(BaseHandler):
             else:
                 self.render("login.html",
                     title = blog_name,
-                    blog_url = blog_url,
-                    blog_name = blog_name,
                     not_login = True
                     )
         else:
             self.render("login.html",
                 title = blog_name,
-                blog_url = blog_url,
-                blog_name = blog_name,
                 not_login = False
                 )
 
@@ -72,20 +67,55 @@ class LoginHandler(BaseHandler):
         else:
             self.render("login.html",
                 title = blog_name,
-                blog_url = blog_url,
-                blog_name = blog_name,
-                not_login = True
+                not_login = True,
                 )
 
-class AdminHandler(BaseHandler):
+class LogoutHandler(BaseHandler):
+    def get(self):
+        user = self.get_current_user()
+        if not user:
+            self.redirect("/")
+        self.clear_cookie("username")
+        self.clear_cookie("token")
+        self.redirect("/")
 
+class AdminHandler(BaseHandler):
     @tornado.web.authenticated
     def get(self):
         self.render("admin.html",
-            title = blog_name,
-            blog_url = blog_url,
-            blog_name = blog_name,
+            title = blog_name + " | Admin",
             blog_author = blog_author,
-            articlesList = get_all_articles()
+            articlesList = get_all_articles(),
+            success = False,
             )
-        
+
+class NewArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self):
+        self.render("editor.html",
+            title = blog_name + " | New",
+            new = True,
+            categories = get_all_categories(),
+            )
+
+class EditArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self, article_id):
+        self.render("editor.html",
+            title = blog_name + " | Edit",
+            new = False,
+            )
+
+class DelArticleHandler(BaseHandler):
+    @tornado.web.authenticated
+    def get(self, article_id):
+        delete_article(article_id)
+        self.render("admin.html",
+            title = blog_name,
+            blog_author = blog_author,
+            articlesList = get_all_articles(),
+            success = True,
+            message = "Successful to delete an article"
+            )
+        time.sleep(3)
+        self.redirect("/admin")
---
+++ libs/handler.py
@@ -0,0 +1,33 @@
+import tornado.web
+
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        username = self.get_secure_cookie("username")
+        if not username:
+            return None
+        return username
+
+    def render_string(self, template_name, title, **kwargs):
+        base_value = dict(
+            blog_url = self.application.settings["blog_url"],
+            blog_name = self.application.settings["blog_name"],
+            title = title,
+            )
+        template_path = self.get_template_path()
+        if not template_path:
+            frame = sys._getframe(0)
+            web_file = frame.f_code.co_filename
+            while frame.f_code.co_filename == web_file:
+                frame = frame.f_back
+            template_path = os.path.dirname(frame.f_code.co_filename)
+        with tornado.web.RequestHandler._template_loader_lock:
+            if template_path not in tornado.web.RequestHandler._template_loaders:
+                loader = self.create_template_loader(template_path)
+                tornado.web.RequestHandler._template_loaders[template_path] = loader
+            else:
+                loader = tornado.web.RequestHandler._template_loaders[template_path]
+        t = loader.load(template_name)
+        namespace = self.get_template_namespace()
+        namespace.update(kwargs)
+        namespace.update(base_value)
+        return t.generate(**namespace)
