aff5c4c57d2cbe035ef4ccd8c39f44887295fa60
JmPotato <ghzpotato@gmail.com>
2024-01-15 11:32:59 +0800

    Bump some dependencies to the latest versions
    Signed-off-by: JmPotato <ghzpotato@gmail.com>

--- Pomash/libs/handler.py
+++ Pomash/libs/handler.py
@@ -6,38 +6,38 @@ import mistune
 import tornado.web
 
 from .models import *
-from .markdown import *
+from .markdown import MarkdownRender
 from urllib.parse import unquote, quote
 from tornado.escape import to_unicode, xhtml_escape
 
 
 class BaseHandler(tornado.web.RequestHandler):
     def get_pure_title(self, title):
-        return re.sub('''<("[^"]*"|'[^']*'|[^'">])*>''', "", title).strip()
+        return re.sub("""<("[^"]*"|'[^']*'|[^'">])*>""", "", title).strip()
 
     def escape_string(self, s):
         return xhtml_escape(s)
 
     def description(self, text):
         if len(text) <= 200:
-            return re.sub('(<.*?>)', '', text).replace('\n', ' ')[:int(len(text)/2-4)] + '...'
+            return (
+                re.sub("(<.*?>)", "", text).replace("\n", " ")[: int(len(text) / 2 - 4)]
+                + "..."
+            )
         elif len(text) > 200:
-            return re.sub('(<.*?>)', '', text).replace('\n', ' ')[:195] + '...'
+            return re.sub("(<.*?>)", "", text).replace("\n", " ")[:195] + "..."
 
     def md_to_html(self, text):
         text = to_unicode(text)
-        renderer = MyRenderer()
-        md = mistune.create_markdown(
-            renderer=renderer,
-            plugins=['strikethrough']
-        )
+        renderer = MarkdownRender()
+        md = mistune.create_markdown(renderer=renderer, plugins=["strikethrough"])
         return md(text)
 
     def urlencode(self, text):
-        return quote(text.encode('utf8'))
+        return quote(text.encode("utf8"))
 
     def urldecode(self, text):
-        return unquote(text.encode('utf8'))
+        return unquote(text.encode("utf8"))
 
     def get_custom_page(self):
         return get_all_pages()
@@ -50,23 +50,18 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def get_error_html(self, status_code, **kwargs):
         if status_code == 404:
-            self.render("404.html",
-                        title="404 Page Not Found",
-                        )
+            self.render(
+                "404.html",
+                title="404 Page Not Found",
+            )
         else:
             try:
-                exception = "%s\n\n%s" % (
-                    kwargs["exception"],
-                    traceback.format_exc()
-                )
+                exception = "%s\n\n%s" % (kwargs["exception"], traceback.format_exc())
                 if self.settings.get("debug"):
-                    self.set_header('Content-Type', 'text/plain')
+                    self.set_header("Content-Type", "text/plain")
                     for line in exception:
                         self.write(line)
                 else:
                     self.write("oOps...! I made ​​a mistake... ")
             except Exception:
-                return super(BaseHandler, self).get_error_html(
-                    status_code,
-                    **kwargs
-                )
+                return super(BaseHandler, self).get_error_html(status_code, **kwargs)
--- Pomash/libs/markdown.py
+++ Pomash/libs/markdown.py
@@ -12,47 +12,43 @@ from .utils import trim
 from .. import dark_mode, pygments_style_light, pygments_style_dark
 
 
-class MyRenderer(mistune.HTMLRenderer):
+class MarkdownRender(mistune.HTMLRenderer):
     def emphasis(self, text):
-        return '%s' % text
+        return "%s" % text
 
-    def block_code(self, code, language=None):
-        if language:
-            try:
-                lexer = get_lexer_by_name(language, stripall=True)
-            except:
-                return "<pre><code>%s</code></pre>" % code.strip()
-        else:
+    def block_code(self, code, info=None):
+        if info is None:
             return "<pre><code>%s</code></pre>" % code.strip()
+        lexer = get_lexer_by_name(info, stripall=True)
 
         try:
             get_style_by_name(trim(pygments_style_light))
             light_style = trim(pygments_style_light)
         except:
-            light_style = 'pastie'
+            light_style = "pastie"
 
         try:
             get_style_by_name(trim(pygments_style_dark))
             dark_style = trim(pygments_style_dark)
         except:
-            dark_style = 'monokai'
+            dark_style = "monokai"
 
         formatter = HtmlFormatter(
             style=dark_style if dark_mode else light_style,
             noclasses=True,
-            linenos=False
+            linenos=False,
         )
 
         return '<div class="highlight-pre">%s</div>' % highlight(code, lexer, formatter)
 
     def autolink(self, link, is_email):
         if is_email:
-            mailto = "".join(['&#%d;' % ord(letter) for letter in "mailto:"])
-            email = "".join(['&#%d;' % ord(letter) for letter in link])
+            mailto = "".join(["&#%d;" % ord(letter) for letter in "mailto:"])
+            email = "".join(["&#%d;" % ord(letter) for letter in link])
             url = mailto + email
-            return '<a href="%(url)s">%(link)s</a>' % {'url': url, 'link': email}
+            return '<a href="%(url)s">%(link)s</a>' % {"url": url, "link": email}
 
-        title = link.replace('http://', '').replace('https://', '')
+        title = link.replace("http://", "").replace("https://", "")
         if len(title) > 30:
             title = title[:24] + "..."
         return '<a href="%s">%s</a>' % (link, title)
